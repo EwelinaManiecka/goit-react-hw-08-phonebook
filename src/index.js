cb4ef7e59fd9a40d2b30ae2c4928290117db88fe
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-02-19 11:31:57 +0100

    AuthNav
    AuthNav add link to goit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
